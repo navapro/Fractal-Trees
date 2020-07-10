2eb6b4f92f43c49a5963fcccfa123802cd955458
NK <45469562+navapro@users.noreply.github.com>
2020-07-10 16:17:29 -0600

    Added right branch and left branch

--- branch.js
+++ branch.js
@@ -10,6 +10,23 @@ class Branch {
         stroke (255);
         line(this.start.x, this.start.y, this.end.x, this.end.y);
     }
-    
+    branchRight(){
+        let dir = p5.Vector.sub(this.end,this.start);
+        dir.rotate(PI/4);
+        dir.mult(0.7);
+        let newEnd = p5.Vector.add(this.end,dir);
+
+        let right = new Branch (this.end, newEnd);
+        return right;
+    }
+    branchLeft(){
+        let dir = p5.Vector.sub(this.end,this.start);
+        dir.rotate(-PI/4);
+        dir.mult(0.7);
+        let newEnd = p5.Vector.add(this.end,dir);
+
+        let left = new Branch (this.end, newEnd);
+        return left;
+    }
 }
 
