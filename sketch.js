846a02e89a46cd454875d91bd883c7615ec52841
NK <45469562+navapro@users.noreply.github.com>
2020-07-10 16:17:36 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,33 @@
-function setup(){
- createCanvas(400,400);
-}
-function draw(){
-  background(0);
+let angle;
+let slider;
+let trees = [];
 
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  slider = createSlider(0, PI * 2, PI / 4, 0.001);
+  slider.position(width / 2, height / 4);
+  let a = createVector(width / 2, height);
+  let b = createVector(width / 2, height - height / 5);
+  trees.push(new Branch(a, b));
+  trees.push(trees[0].branchRight());
+  trees.push(trees[0].branchLeft());
+}
+function draw() {
+  background(50);
+  angle = slider.value();
+  for (let i = 0; i < trees.length; i++) {
+    trees[i].show();
+  }
+
+}
+function mousePressed(){
+  for (let i = trees.length -1; i >= 0; i--){
+    if (!trees[i].done){
+      trees.push(trees[i].branchRight());
+      trees.push(trees[i].branchLeft());
+    }
+
+    trees[i].done = true;
+
+  }
 }
